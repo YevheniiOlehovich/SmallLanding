eded92a323e1b6d155b637c589cce7822c696284
Yevhenii Khyzhniak <keildra258@gmail.com>
2024-12-20 00:18:34 +0200

    added highlight list

---
+++ src/helpers/index.js
@@ -0,0 +1,72 @@
+export const highlights = [
+    {
+        name: 'Chassol',
+        event: 'CREATING A TRACK FROM A TO B',
+        date: '1 Dec 2022 - 08:00PM'
+    },
+    {
+        name: 'Low Jack',
+        event: 'Baiser Mortel avec Low Jack, Le Diouck',
+        date: '23 Nov 2022 - 07:00PM'
+    },
+    {
+        name: 'MKL',
+        event: 'INSIDE TEXTURES (2 HOURS)',
+        date: '16 Oct 2022 - 01:00PM'
+    },
+    {
+        name: 'Biesmans',
+        event: 'Track breakdown: "Vanishing Flowers"',
+        date: '13 Oct 2022 - 05:00PM'
+    },
+    {
+        name: 'Frank Wiedemann',
+        event: 'Track Breakdown: "REJ"',
+        date: '27 Sep 2022 - 06:00PM'
+    },
+    {
+        name: 'Fat Tony',
+        event: 'In-Studio w/ Steel Tipped Dove',
+        date: '17 Sep 2022 - 09:00PM'
+    },
+    {
+        name: 'JB Dunckel',
+        event: 'Track Breakdown: "Spark"',
+        date: '13 Sep 2022 - 05:00PM'
+    },
+    {
+        name: 'Luke Slater',
+        event: 'Limitations in creation - Nuits Sonores workshop',
+        date: '25 May 2022 - 11:23AM'
+    },
+    {
+        name: 'Para One',
+        event: 'Scoring films',
+        date: '8 May 2022 - 04:46PM'
+    },
+    {
+        name: 'Paloma Colombe',
+        event: 'Paloma Colombe invites Anna Olekhnovych',
+        date: '11 Apr 2022 - 04:53PM'
+    },
+    {
+        name: 'Marina Trench',
+        event: 'Remote Digging - Marina Trench Take Over',
+        date: '31 Mar 2022 - 05:21PM'
+    },
+    {
+        name: 'DJ TENNIS',
+        event: 'DJ Tennis & Joseph Ashworth - Arrangement & Production workshop',
+        date: '28 Jan 2022 - 06:07PM'
+    },
+    {
+        name: 'NSDOS',
+        event: 'Creative Coding & Music by NSDOS',
+        date: '26 Jan 2022 - 04:10PM'
+    },
+    {
+        name: 'Lauer',
+        event: 'Track Breakdown: Sueno Hebreo (Lauer Remix)',
+        date: '20 Jan 2022 - 06:28PM'
+    },
+]
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
+import { highlights } from "./helpers";
+
 document.addEventListener("DOMContentLoaded", function () {
     const swiper = new Swiper('.swiper', {
         modules: [Navigation], 
@@ -20,4 +22,29 @@ document.addEventListener("DOMContentLoaded", function () {
         slidesPerView: 'auto', 
         spaceBetween: 20,  
     });
+});
+
+const list = document.querySelector('.highlights__list');
+
+highlights.forEach(item => {
+    const li = document.createElement('li');
+    li.classList.add('highlights__item');
+    
+    const name = document.createElement('div');
+    name.classList.add('highlights__item-name');
+    name.textContent = item.name;
+
+    const event = document.createElement('div');
+    event.classList.add('highlights__item-event');
+    event.textContent = item.event;
+
+    const time = document.createElement('div');
+    time.classList.add('highlights__item-time');
+    time.textContent = item.date;
+
+    li.appendChild(name);
+    li.appendChild(event);
+    li.appendChild(time);
+
+    list.appendChild(li);
 });
