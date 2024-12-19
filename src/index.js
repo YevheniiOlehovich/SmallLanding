cf0b8d97e65f162bde9a57b80ce319a2cf6623f9
Yevhenii Khyzhniak <keildra258@gmail.com>
2024-12-19 23:32:48 +0200

    added slider

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@ import "@babel/polyfill";
 import './index.html';
 import './index.scss';
 
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper('.swiper', {
+        modules: [Navigation], 
+        loop: true, 
+        
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        slidesPerView: 'auto', 
+        spaceBetween: 20,  
+    });
+});
